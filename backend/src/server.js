2e506129c652654a0dc6b71274994de40559e359
Matheus Vidal <matheuszvidal@DESKTOP-BGNLV1T.localdomain>
2022-08-24 04:55:05 -0400

    backend finalizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const routes = require('./routes')
 
 const server = express()
 
-server.get('/', (req, res) => {
-  return res.json(`Hello ${req.query.name}` )
+mongoose.connect('mongodb+srv://vidal:vidal@cluster0.i3ad3km.mongodb.net/trynder?retryWrites=true&w=majority', {
+  useNewUrlParser: true
 })
+
+server.use(cors())
+server.use(express.json())
+server.use(routes)
 
 server.listen(3333)
